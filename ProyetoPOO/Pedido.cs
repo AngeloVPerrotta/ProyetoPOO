8d47e52f28fc51f896493ecc5d5a4f3f0894caf9
Ramiro Cordero <corderoramiro14@gmail.com>
2025-06-21 17:09:25 -0300

    Agregue atributos, constructor y un metodo

--- ProyetoPOO/Pedido.cs
+++ ProyetoPOO/Pedido.cs
@@ -11,5 +11,34 @@ namespace ProyetoPOO
         //Atributos: idPedido, producto(o código), cantidad, fechaPedido, fechaProbableEntrega, costoUnitario o costoTotal, proveedor, empleadoResponsable, estado(pendiente, recibido, cancelado).
         //Métodos: calcularCostoTotal(), cambiarEstado(...).
 
+        public int idPedido { get; set; }
+        public int Producto { get; set; }
+        public int Cantidad { get; set; }
+        public int FechaPedido { get; set; }
+        public int FechaProbableEntrega { get; set; }
+        public int CostoTotal { get; set; }
+        public string Proveedor { get; set; }
+        public string EmpleadoResponsable { get; set; }
+        public string Estado { get; set; }
+
+        public Pedido(int idpedido, int producto, int cantidad, int fechapedido, int fechaprobableentrega, int costototal, string proveedor, string empleadoresponsable, string estado)
+        {
+            idPedido = idpedido;
+            Producto = producto;
+            Cantidad = cantidad;
+            FechaPedido = fechapedido;
+            FechaProbableEntrega = fechaprobableentrega;
+            CostoTotal = costototal;
+            Proveedor = proveedor;
+            EmpleadoResponsable = empleadoresponsable;
+            Estado = estado;
+        }
+
+        //Metodo 
+        //public string CalcularCostoTotal()
+        //{
+        //  return $"Id pedido: {idPedido}, Producto: {Producto}, Cantidad: {Cantidad}, Fecha de pedido: {FechaPedido}, Fecha probable de entrega: {FechaProbableEntrega}, Costo: {CostoTotal}"; 
+        //}
+
     }
 }
