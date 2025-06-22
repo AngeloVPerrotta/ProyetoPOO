94e4fd8065dca3700e56e9e5b0baab0dfa4341aa
Angelo <perrottangelo340@gmail.com>
2025-06-22 13:15:43 -0300

    Termine de hacer clases, falta crear metodos, atributos listos, forms creados, capas creadas y separadas.

--- DAL/CSVHelper.cs
+++ DAL/CSVHelper.cs
@@ -10,6 +10,6 @@ namespace DAL
     {
         //guardar y cargar listas desde csv
         //metodos para GuardarProductos(), LeerProductos(), GuardarProveedores(), LeerProveedores()
-        //agregar Repositorio<T<T> para guardar y cargar listas de objetos genéricos
+        //agregar Repositorio<T> para guardar y cargar listas de objetos genéricos
     }
 }
