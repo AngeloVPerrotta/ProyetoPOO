d31c99117cfeeb805a94281cfb3e2714a721a591
Franco <hourcade.franco@gmail.com>
2025-07-06 11:00:28 -0300

    Avances en el funcionamiento del form proveedores. Falta el acceso a archivos

--- ProyetoPOO/FormProveedores.Designer.cs
+++ ProyetoPOO/FormProveedores.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridViewProv = new System.Windows.Forms.DataGridView();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBoxContacto = new System.Windows.Forms.TextBox();
-            this.buttonElimProv = new System.Windows.Forms.Button();
-            this.buttonAgregarProv = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBoxRubro = new System.Windows.Forms.TextBox();
             this.textBoxProv = new System.Windows.Forms.TextBox();
+            this.textBoxRubro = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonAgregarProv = new System.Windows.Forms.Button();
+            this.buttonElimProv = new System.Windows.Forms.Button();
+            this.textBoxContacto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridViewProv = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProv)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProv)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,14 +54,92 @@
             this.panel1.Size = new System.Drawing.Size(392, 378);
             this.panel1.TabIndex = 0;
             // 
-            // dataGridViewProv
+            // textBoxProv
             // 
-            this.dataGridViewProv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewProv.Location = new System.Drawing.Point(18, 26);
-            this.dataGridViewProv.Name = "dataGridViewProv";
-            this.dataGridViewProv.Size = new System.Drawing.Size(354, 338);
-            this.dataGridViewProv.TabIndex = 0;
-            this.dataGridViewProv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProv_CellContentClick);
+            this.textBoxProv.Location = new System.Drawing.Point(113, 56);
+            this.textBoxProv.Name = "textBoxProv";
+            this.textBoxProv.Size = new System.Drawing.Size(100, 20);
+            this.textBoxProv.TabIndex = 0;
+            // 
+            // textBoxRubro
+            // 
+            this.textBoxRubro.Location = new System.Drawing.Point(113, 82);
+            this.textBoxRubro.Name = "textBoxRubro";
+            this.textBoxRubro.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRubro.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Proveedor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rubro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(57, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Agregar nuevo proveedor";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // buttonAgregarProv
+            // 
+            this.buttonAgregarProv.Location = new System.Drawing.Point(35, 155);
+            this.buttonAgregarProv.Name = "buttonAgregarProv";
+            this.buttonAgregarProv.Size = new System.Drawing.Size(178, 23);
+            this.buttonAgregarProv.TabIndex = 5;
+            this.buttonAgregarProv.Text = "Agregar Proveedor";
+            this.buttonAgregarProv.UseVisualStyleBackColor = true;
+            this.buttonAgregarProv.Click += new System.EventHandler(this.buttonAgregarProv_Click);
+            // 
+            // buttonElimProv
+            // 
+            this.buttonElimProv.Location = new System.Drawing.Point(35, 184);
+            this.buttonElimProv.Name = "buttonElimProv";
+            this.buttonElimProv.Size = new System.Drawing.Size(178, 23);
+            this.buttonElimProv.TabIndex = 6;
+            this.buttonElimProv.Text = "Eliminar proveedor";
+            this.buttonElimProv.UseVisualStyleBackColor = true;
+            // 
+            // textBoxContacto
+            // 
+            this.textBoxContacto.Location = new System.Drawing.Point(113, 109);
+            this.textBoxContacto.Name = "textBoxContacto";
+            this.textBoxContacto.Size = new System.Drawing.Size(100, 20);
+            this.textBoxContacto.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Contacto";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Mostrar proveedores";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -81,91 +159,14 @@
             this.panel2.TabIndex = 1;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
-            // button1
+            // dataGridViewProv
             // 
-            this.button1.Location = new System.Drawing.Point(35, 213);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Mostrar proveedores";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 116);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Contacto";
-            // 
-            // textBoxContacto
-            // 
-            this.textBoxContacto.Location = new System.Drawing.Point(113, 109);
-            this.textBoxContacto.Name = "textBoxContacto";
-            this.textBoxContacto.Size = new System.Drawing.Size(100, 20);
-            this.textBoxContacto.TabIndex = 7;
-            // 
-            // buttonElimProv
-            // 
-            this.buttonElimProv.Location = new System.Drawing.Point(35, 184);
-            this.buttonElimProv.Name = "buttonElimProv";
-            this.buttonElimProv.Size = new System.Drawing.Size(178, 23);
-            this.buttonElimProv.TabIndex = 6;
-            this.buttonElimProv.Text = "Eliminar proveedor";
-            this.buttonElimProv.UseVisualStyleBackColor = true;
-            // 
-            // buttonAgregarProv
-            // 
-            this.buttonAgregarProv.Location = new System.Drawing.Point(35, 155);
-            this.buttonAgregarProv.Name = "buttonAgregarProv";
-            this.buttonAgregarProv.Size = new System.Drawing.Size(178, 23);
-            this.buttonAgregarProv.TabIndex = 5;
-            this.buttonAgregarProv.Text = "Agregar Proveedor";
-            this.buttonAgregarProv.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(57, 26);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Agregar nuevo proveedor";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 89);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Rubro";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Proveedor";
-            // 
-            // textBoxRubro
-            // 
-            this.textBoxRubro.Location = new System.Drawing.Point(113, 82);
-            this.textBoxRubro.Name = "textBoxRubro";
-            this.textBoxRubro.Size = new System.Drawing.Size(100, 20);
-            this.textBoxRubro.TabIndex = 1;
-            // 
-            // textBoxProv
-            // 
-            this.textBoxProv.Location = new System.Drawing.Point(113, 56);
-            this.textBoxProv.Name = "textBoxProv";
-            this.textBoxProv.Size = new System.Drawing.Size(100, 20);
-            this.textBoxProv.TabIndex = 0;
+            this.dataGridViewProv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProv.Location = new System.Drawing.Point(18, 26);
+            this.dataGridViewProv.Name = "dataGridViewProv";
+            this.dataGridViewProv.Size = new System.Drawing.Size(354, 338);
+            this.dataGridViewProv.TabIndex = 0;
+            this.dataGridViewProv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProv_CellContentClick);
             // 
             // FormProveedores
             // 
@@ -178,9 +179,9 @@
             this.Text = "PROVEEDORES";
             this.Load += new System.EventHandler(this.FormProveedores_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProv)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProv)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -188,17 +189,17 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridViewProv;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TextBox textBoxRubro;
         private System.Windows.Forms.TextBox textBoxProv;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxRubro;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button buttonElimProv;
         private System.Windows.Forms.Button buttonAgregarProv;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonElimProv;
         private System.Windows.Forms.TextBox textBoxContacto;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridViewProv;
     }
 }
--- ProyetoPOO/FormProveedores.cs
+++ ProyetoPOO/FormProveedores.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics.Eventing.Reader;
 
 namespace ProyetoPOO
 {
@@ -15,30 +16,23 @@ namespace ProyetoPOO
     {
         private string rutacsv = "Proveedores.csv";
         private DataTable tablaProv;
+        private bool okay() 
+        {
+            bool ok = false;
+            if (textBoxProv.Text != "" && textBoxRubro.Text != "" && textBoxContacto.Text != "") 
+            {
+                ok = true;
+            }
+            return ok;
+        }
         public FormProveedores()
         {
             InitializeComponent();
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        }           
         private void FormProveedores_Load(object sender, EventArgs e)
         {
-
-        }
-        
-        private void dataGridViewProv_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }
+            inicializartabla();            
+        }               
         private void inicializartabla() 
         {
             tablaProv = new DataTable();
@@ -47,18 +41,88 @@ namespace ProyetoPOO
             tablaProv.Columns.Add("Contacto");
             dataGridViewProv.DataSource = tablaProv;
         }
+        private void escriba(string texto) 
+        {
+            tablaProv.Rows.Add(texto);
+        }
         private void cargarDesdeCSV() 
         {
-            if (File.Exists(rutacsv)) 
+            FileStream fs = new FileStream("Proveedores.csv",FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);            
+            string linea = "";
+            string legajo = "";
+            string[] vl = new string[0];
+            linea = sr.ReadLine();
+            linea = sr.ReadLine();
+            while (linea != null) 
             {
-                var lineas = File.ReadAllLines(rutacsv);
-                foreach (var line in lineas) 
+                vl = linea.Split(';');
+                string texto = vl[0] + vl[1] + vl[2] + vl[3];
+                legajo = vl[0];
+                escriba(texto);
+                linea = sr.ReadLine();
+                if (linea != null) 
                 {
-                    var datos = line.Split(';');
-                    if (datos.Length == 3) 
-                    {
-                        tablaProv.Rows.Add(datos);
-                    }
+                    vl = linea.Split(';');
+                }
+                sr.Close();
+                fs.Close();
+            }
+            //if (File.Exists(rutacsv)) 
+            //{
+            //    var lineas = File.ReadAllLines(rutacsv);
+            //    foreach (var line in lineas) 
+            //    {
+            //        var datos = line.Split(';');
+            //        if (datos.Length == 3) 
+            //        {
+            //            tablaProv.Rows.Add(datos);
+            //        }
+            //    }
+            //}
+        }
+
+        private void buttonAgregarProv_Click(object sender, EventArgs e)
+        {
+            if (okay())
+            {
+                string[] vectorlinea = new string[3];
+                vectorlinea[0] = textBoxProv.Text;
+                vectorlinea[1] = textBoxRubro.Text;
+                vectorlinea[2] = textBoxContacto.Text;
+                tablaProv.Rows.Add(vectorlinea);
+                limpiarCampos();
+            }
+            else 
+            {
+                MessageBox.Show("Complete todos los campos");
+            }
+        }
+        private void limpiarCampos()
+        {
+            textBoxProv.Text = "";
+            textBoxRubro.Text = "";
+            textBoxContacto.Text = "";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridViewProv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var confirm = MessageBox.Show("¿Deseas eliminar esta fila?", "Confirmar eliminación", MessageBoxButtons.YesNo);
+                if (confirm == DialogResult.Yes)
+                {
+                    dataGridViewProv.Rows.RemoveAt(e.RowIndex);
                 }
             }
         }
